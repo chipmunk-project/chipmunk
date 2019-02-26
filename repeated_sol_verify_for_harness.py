9185b942d186fac7fbf01af406317d1f6769ddce
Xiangyu Gao <xg673@nyu.edu>
2019-02-25 21:13:39 -0500

    Merge repeated_sol_verify_for_harness_counterexample.py and repeated_sol_verify_for_harness_negation_assert.py to repeated_sol_verify_for_harness.py

--- repeated_sol_verify_for_harness.py
+++ repeated_sol_verify_for_harness.py
@@ -15,8 +15,8 @@ def get_num_pkt_fields_and_state_vars(program):
   return (max(pkt_fields) + 1, max(state_vars) + 1)
 
 
-if (len(sys.argv) < 8):#This part may need change with the chipmunk.py file
-  print("Usage: python3 " + sys.argv[0] + " <program file> <alu file> <number of pipeline stages> <number of stateless/stateful ALUs per stage> <codegen/optverify> <sketch_name (w/o file extension)> <parallel/serial>" )
+if (len(sys.argv) < 9):#This part may need change with the chipmunk.py file
+  print("Usage: python3 " + sys.argv[0] + " <program file> <alu file> <number of pipeline stages> <number of stateless/stateful ALUs per stage> <codegen/optverify> <sketch_name (w/o file extension)> <parallel/serial> <counterexample_version/negationassert_version>" )
   sys.exit(1)
 else:
   start = time.time()
@@ -31,7 +31,8 @@ else:
   assert(mode == "codegen")
   sketch_name          = str(sys.argv[6])
   parallel_or_serial   = str(sys.argv[7])
-
+  version = str(sys.argv[8])
+  assert((version == "counterexample_version") or (version == "negationassert_version"))
 # Initialize jinja2 environment for templates
 env = Environment(loader = FileSystemLoader('./templates'), undefined = StrictUndefined)
 
@@ -72,38 +73,53 @@ else:
     original_sketch_file_string    = open(sketch_name + "_codegen.sk","r").read()
     count = 0
     while(1):
-      hole_value_file_string         = open("/tmp/result.sk","r").read()
-      open("/tmp/result.sk","w").close()
-      begin_pos = hole_value_file_string.find('int')
-      end_pos = hole_value_file_string.rfind(';')
-      hole_value_file_string = hole_value_file_string[begin_pos:end_pos+1]
-      #rearrange the format (int x=1; int y=2;) to (x==1 && y==2 && 1)
-      hole_value_file_string = hole_value_file_string.replace("int","")
-      hole_value_file_string = hole_value_file_string.replace("=","==")
-      hole_value_file_string = hole_value_file_string.replace(";","&&")
-      hole_value_file_string = hole_value_file_string + "1"
-      #find the position of harness
-      begin_pos = original_sketch_file_string.find('harness')
-      begin_pos = original_sketch_file_string.find('assert',begin_pos)
-      #add function assert here
-#      (ret_code_sketch_with_counter_example, output_with_counter_example) = subprocess.getstatusoutput("sketch -V 3 --debug-cex " + "/tmp/new_sketch.sk")
-#      input_values = re.findall('\((\d+)\)' , output_with_counter_example)
-#      hits_pkt = re.findall("pkt_\d+",output_with_counter_example)
-#      hits_state = re.findall("state_\d+",output_with_counter_example)
-#     counter_example_input = ""
-#      for i in range(len(input_values)-1):
-#        counter_example_input += input_values[i] + ", "
-#      counter_example_input += hits[len(input_values)-1]
-#     "assert pipeline(" + "x_" + str(round)  + ")" + " == " + "program(" + "x_" + str(round)  + ");\n"
-      
-      original_sketch_file_string = original_sketch_file_string[0:begin_pos] + "assert(!(" + hole_value_file_string + "));\n" + original_sketch_file_string[begin_pos:]
-     
+      print(count)
+      if (version == "negationassert_version"):
+        hole_value_file_string         = open("/tmp/result.sk","r").read()
+        open("/tmp/result.sk","w").close()
+        begin_pos = hole_value_file_string.find('int')
+        end_pos = hole_value_file_string.rfind(';')
+        hole_value_file_string = hole_value_file_string[begin_pos:end_pos+1]
+        #rearrange the format (int x=1; int y=2;) to (x==1 && y==2 && 1)
+        hole_value_file_string = hole_value_file_string.replace("int","")
+        hole_value_file_string = hole_value_file_string.replace("=","==")
+        hole_value_file_string = hole_value_file_string.replace(";","&&")
+        hole_value_file_string = hole_value_file_string + "1"
+        #find the position of harness
+        begin_pos = original_sketch_file_string.find('harness')
+        begin_pos = original_sketch_file_string.find('assert',begin_pos)
+        original_sketch_file_string = original_sketch_file_string[0:begin_pos] + "assert(!(" + hole_value_file_string + "));\n" + original_sketch_file_string[begin_pos:]
+      else:
+        #find the position of harness
+        begin_pos = original_sketch_file_string.find('harness')
+        begin_pos = original_sketch_file_string.find('assert',begin_pos)
+
+        #add function assert here
+        (ret_code_sketch_with_counter_example, output_with_counter_example) = subprocess.getstatusoutput("sketch -V 3 --debug-cex --bnd-inbits=10 " + sketch_name + "_codegen_with_hole_value.sk")
+        input_values = re.findall("has value \d+= " + '\((\d+)\)' , output_with_counter_example)
+        hits_pkt = re.findall("pkt_\d+",output_with_counter_example)
+        hits_state = re.findall("state_\d+",output_with_counter_example)
+        print(input_values)
+        print("pkt: ",hits_pkt)
+        print("state: ", hits_state)
+        counter_example_definition = "|StateAndPacket| x_" + str(count) + " = |StateAndPacket|(\n"
+        for i in range(len(hits_pkt)):
+          counter_example_definition += hits_pkt[i] + " = " + input_values[i] + ','
+        for i in range(len(hits_state)-1):
+          counter_example_definition += hits_state[i] + " = " + input_values[len(hits_pkt)+i] + ','
+        counter_example_definition += hits_state[len(hits_state)-1] + " = " + input_values[len(input_values)-1] + ");\n"
+
+        counter_example_assert = "assert pipeline(" + "x_" + str(count)  + ")" + " == " + "program(" + "x_" + str(count)  + ");\n"
+        original_sketch_file_string = original_sketch_file_string[0:begin_pos] + counter_example_definition + counter_example_assert + original_sketch_file_string[begin_pos:]  
+
       new_sketch = open("/tmp/new_sketch.sk","w")
       new_sketch.write(original_sketch_file_string)
       new_sketch.close()
-
+#success      print("Hello")
       (ret_code1, output) = subprocess.getstatusoutput("time sketch -V 12 --slv-seed=1 --slv-parallel --bnd-inbits=2 --bnd-int-range=50 " + new_sketch.name)
+      print("Hello")
       hole_value_string = ""
+#Failed      print("Hello1")
       if (ret_code1 == 0):
         hole_value_file = open("/tmp/result.sk","w")
         for hole_name in sketch_generator.hole_names_:
@@ -132,5 +148,4 @@ else:
         print("total time in seconds: ", end-start)
         print("total while loop: ", count)
         sys.exit(1)
-        
         
