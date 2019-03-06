cdeea9ca14cd4e6f4b3c61843a7f7b4b4bf9908c
XiangyuG <39160477+XiangyuG@users.noreply.github.com>
2019-03-06 10:40:56 -0500

    Some change to repeated_solver.py (#23)
    * First draft of repeated solution verifier with harness instead of implements.
    
    * Add repeated_sol_verify_for_harness_counterexample.py and repeated_sol_verify_for_harness_negation_assert.py
    which use two different ways to find the hole values
    
    * Change the testing command line to generate counterexample
    
    * Merge repeated_sol_verify_for_harness_counterexample.py and repeated_sol_verify_for_harness_negation_assert.py to repeated_sol_verify_for_harness.py
    
    * Rename Xiangyu's file to make it shorter.
    
    * Rename result.sk to result.holes
    
    * Add one more explicit print during while loop
    
    * Use another way to run sketch because the former way got stuck
    
    * Change the name of negationassert to hole_elimination; change the filename generated in /tmp
    
    * Fix a typo
    
    * Fix one bug which always generate the same counterexample because of solving one file without changing it
    
    * Replace sol_verify.py as a function in repeated_solver.py
    
    * A better way to get counterexample

--- sol_verify.py
+++ sol_verify.py
@@ -3,6 +3,7 @@
 
 import sys
 import subprocess
+"""
 from pathlib import Path
 
 if (len(sys.argv) != 3):
@@ -39,8 +40,38 @@ with open(sketch_file_name, "w") as sketch_file:
 (ret_code, output) = subprocess.getstatusoutput("sketch --bnd-inbits=10 " +
                                                 sketch_file_name)
 if (ret_code != 0):
-    print("Sketch failed.")
-    sys.exit(1)
+  #failed
+  print("Sketch failed.")
+  sys.exit(1)
 else:
-    print("Sketch succeeded.")
-    sys.exit(0)
+  print("Sketch succeeded.")
+  sys.exit(0)
+"""
+def sol_verify(original_sketch_file,hole_value_file):
+
+  original_sketch_file_string    = open(str(original_sketch_file),"r").read()
+  hole_value_file_string         = open(str(hole_value_file),"r").read()
+
+  #remove the hole definition in original_sketch_file
+  original_sketch_file_string = original_sketch_file_string[original_sketch_file_string.rfind('??'):]
+  original_sketch_file_string = original_sketch_file_string[original_sketch_file_string.find(';')+1:]
+
+  #remove the redundant code in hole_value_file
+  begin_pos = hole_value_file_string.find('int')
+  end_pos = hole_value_file_string.rfind(';')
+  hole_value_file_string = hole_value_file_string[begin_pos:end_pos+1]
+
+  sketch_file_with_hole_value = hole_value_file_string + original_sketch_file_string
+
+  # Create file and write sketch_harness into it.
+  sketch_file = open(original_sketch_file[0:original_sketch_file.find('.')] + "_with_hole_value.sk", "w")
+  sketch_file.write(sketch_file_with_hole_value)
+  sketch_file.close()
+
+  # Call sketch on it
+  (ret_code, output) = subprocess.getstatusoutput("sketch --bnd-inbits=10 " + sketch_file.name)
+  if (ret_code!=0):
+    #fail print 1
+    return 1
+  else:
+    return 0
