779f40ee293bb60b97d185f02ab04782f00fc27a
Taegyun Kim <k.taegyun@gmail.com>
2019-03-10 17:27:05 -0400

    Add trim_blocks and add lstrip_blocks

--- chipmunk/utils.py
+++ chipmunk/utils.py
@@ -9,20 +9,18 @@ def get_num_pkt_fields_and_state_groups(program):
     """Returns number of packet fields and state groups.
     Use a regex to scan the program and extract the largest packet field index
     and largest state variable index
-
     Args:
         program: The program to read
-
     Returns:
         A tuple of packet field numbers and state variables.
     """
     pkt_fields = [
         int(x) for x in findall(r'state_and_packet.pkt_(\d+)', program)
     ]
-    state_gropus = [
+    state_groups = [
         int(x) for x in findall(r'state_and_packet.state_group_(\d+)', program)
     ]
-    return (max(pkt_fields) + 1, max(state_gropus) + 1)
+    return (max(pkt_fields) + 1, max(state_groups) + 1)
 
 
 def get_hole_dicts(sketch_file):
@@ -41,4 +39,6 @@ def load_jinja2_templates():
     # Initialize jinja2 environment for templates
     return Environment(
         loader=FileSystemLoader(path.join(here, '../templates')),
-        undefined=StrictUndefined)
+        undefined=StrictUndefined,
+        trim_blocks=True,
+        lstrip_blocks=True)
