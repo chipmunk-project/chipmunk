afbad4940f8b30cb85a6d122d19e25016731c319
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-03-10 15:28:17 -0400

    Beginnings of a parallel compiler.

---
+++ parallel_compile.py
@@ -0,0 +1,48 @@
+"""Parallel compiler"""
+from pathlib import Path
+import sys
+import re
+
+from compiler import Compiler
+
+def main(argv):
+    """Main program."""
+    if len(argv) != 6:
+        print("Usage: python3 " + argv[0] +
+              " <program file> <alu file> <number of pipeline stages> " +
+              "<number of stateless/stateful ALUs per stage> " +
+              "<sketch_name (w/o file extension)> ")
+        sys.exit(1)
+
+    program_file = str(argv[1])
+    alu_file = str(argv[2])
+    num_pipeline_stages = int(argv[3])
+    num_alus_per_stage = int(argv[4])
+    sketch_name = str(argv[5])
+
+    compiler = Compiler(program_file, alu_file, num_pipeline_stages,
+                        num_alus_per_stage, sketch_name, "serial")
+
+    # Now fill the appropriate template holes using the components created using
+    # sketch_generator
+    (ret_code, output) = compiler.codegen()
+
+    if ret_code != 0:
+        with open(compiler.sketch_name + ".errors", "w") as errors_file:
+            errors_file.write(output)
+            print("Sketch failed. Output left in " + errors_file.name)
+        sys.exit(1)
+
+    for hole_name in compiler.sketch_generator.hole_names_:
+        hits = re.findall("(" + hole_name + ")__" + r"\w+ = (\d+)", output)
+        assert len(hits) == 1
+        assert len(hits[0]) == 2
+        print("int ", hits[0][0], " = ", hits[0][1], ";")
+    with open(compiler.sketch_name + ".success", "w") as success_file:
+        success_file.write(output)
+        print("Sketch succeeded. Generated configuration is given " +
+              "above. Output left in " + success_file.name)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main(sys.argv)
