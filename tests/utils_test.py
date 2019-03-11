0d3c27ea8f7ae51ce847dc8d001a42a8950f5964
Taegyun Kim <k.taegyun@gmail.com>
2019-03-11 11:45:49 -0400

    Remove unnecessary codes.

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,4 +1,3 @@
-from os import path
 from pathlib import Path
 import unittest
 
@@ -6,10 +5,6 @@ from utils import get_hole_dicts, get_hole_value_assignments
 
 
 class GetHoleValueAssignmentsTest(unittest.TestCase):
-    def setUp(self):
-        self.base_path = path.abspath(path.dirname(__file__))
-        self.data_dir = path.join(self.base_path, "data/")
-
     def test_success(self):
         with self.assertRaises(AssertionError):
             get_hole_value_assignments(["a"], "a__one = 3; a__two = 4")
