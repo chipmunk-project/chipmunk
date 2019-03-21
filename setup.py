843c14e6834596a861fe838a4b9b340e4c449cb4
Taegyun Kim <k.taegyun@gmail.com>
2019-03-21 15:18:49 -0400

    Add setup and init.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='chipmunk-compiler',
+      version='0.1',
+      description='A switch code generator based on end-to-end program ' + \
+      'synthesis.',
+      url='https://github.com/anirudhSK/chipmunk',
+      author='Chipmunk Contributors',
+      packages=find_packages())
