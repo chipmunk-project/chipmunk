8c040a858b172e6189e770414a2f58ed89e84da0
Taegyun Kim <k.taegyun@gmail.com>
2019-03-21 16:26:12 -0400

    Add setuptools hook for antlr.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='chipmunk-compiler',
+setup(name='chipmunk',
       version='0.1',
       description='A switch code generator based on end-to-end program ' + \
       'synthesis.',
       url='https://github.com/anirudhSK/chipmunk',
       author='Chipmunk Contributors',
-      packages=find_packages())
+      packages=find_packages(),
+      setup_requires=['setuptools-antlr'],
+      install_requires=['antlr4-python3-runtime'])
