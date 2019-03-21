d515a460fe55fd93cc8fc01c95c7116d97f6a93c
Taegyun Kim <k.taegyun@gmail.com>
2019-03-21 17:13:14 -0400

    No need to use setuptools-antlr.

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='chipmunk',
-      version='0.1',
-      description='A switch code generator based on end-to-end program ' + \
-      'synthesis.',
-      url='https://github.com/anirudhSK/chipmunk',
-      author='Chipmunk Contributors',
-      packages=find_packages(),
-      setup_requires=['setuptools-antlr'],
-      install_requires=['antlr4-python3-runtime'])
+setup(
+    name='chipmunk',
+    version='0.1',
+    description='A switch code generator based on end-to-end program ' +
+    'synthesis.',
+    url='https://github.com/anirudhSK/chipmunk',
+    author='Chipmunk Contributors',
+    packages=find_packages(exclude=["tests*"]))
