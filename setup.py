d0f224124f2bb473023919e00634e1305df355a5
Taegyun Kim <k.taegyun@gmail.com>
2019-03-22 16:55:25 -0400

    Install works but egg-info doesn't include the generate python files.

--- setup.py
+++ setup.py
@@ -5,7 +5,27 @@ import shutil
 
 from setuptools import setup
 from setuptools import find_packages
-from setuptools.command.install import install
+from setuptools.command.install_lib import install_lib
+
+
+class InstallWrapper(install_lib):
+    def run(self):
+        install_lib.run(self)
+        self._generate_parser()
+
+    def _generate_parser(self):
+        cmd = 'antlr'
+        if sys.platform.startswith('linux'):
+            cmd += '4'
+        assert shutil.which(
+            cmd) is not None, ("Can't find %s executable." % cmd)
+
+        alu_filepath = os.path.join(self.install_dir, "chipmunk/stateful_alu.g4")
+
+        assert os.access(alu_filepath, os.R_OK), "Can't find grammar file: %s" % alu_filepath
+
+        run_args = "antlr %s -Dlanguage=Python3 -visitor -package chipmunk" % alu_filepath
+        subprocess.run(run_args, shell=True, capture_output=True, check=True)
 
 
 setup(
@@ -16,4 +36,5 @@ setup(
     url='https://github.com/anirudhSK/chipmunk',
     author='Chipmunk Contributors',
     packages=find_packages(exclude=["tests*"]),
-    include_package_data=True)
+    include_package_data=True,
+    cmdclass={'install_lib': InstallWrapper})
