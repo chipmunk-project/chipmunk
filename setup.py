9e025054056c0225b6f50d511d37c0ba91588d4d
Taegyun Kim <k.taegyun@gmail.com>
2019-03-22 17:37:23 -0400

    Finally working version of install and uninstall.

--- setup.py
+++ setup.py
@@ -10,22 +10,25 @@ from setuptools.command.build_py import build_py
 
 class InstallWrapper(build_py):
     def run(self):
-        build_py.run(self)
         self._generate_parser()
+        build_py.run(self)
 
     def _generate_parser(self):
         cmd = 'antlr'
         if sys.platform.startswith('linux'):
             cmd += '4'
-        assert shutil.which(
-            cmd) is not None, ("Can't find %s executable." % cmd)
+        assert shutil.which(cmd) is not None, (
+            "Can't find %s executable." % cmd)
 
-        alu_filepath = os.path.join(self.build_lib, "chipmunk/stateful_alu.g4")
+        alu_filepath = "chipmunk/stateful_alu.g4"
+        assert os.access(alu_filepath,
+                         os.R_OK), "Can't find grammar file: %s" % alu_filepath
 
-        assert os.access(alu_filepath, os.R_OK), "Can't find grammar file: %s" % alu_filepath
-
-        run_args = "antlr %s -Dlanguage=Python3 -visitor -package chipmunk" % alu_filepath
-        subprocess.run(run_args, shell=True, capture_output=True, check=True)
+        run_args = [
+            cmd, alu_filepath, '-Dlanguage=Python3', '-visitor', '-package',
+            'chipmunk'
+        ]
+        subprocess.run(run_args, capture_output=True, check=True)
 
 
 setup(
@@ -35,6 +38,6 @@ setup(
     'synthesis.',
     url='https://github.com/anirudhSK/chipmunk',
     author='Chipmunk Contributors',
-    packages=find_packages(exclude=["tests*"]),
+    packages=find_packages(exclude=["tests*", "*.interp", "*.tokens"]),
     include_package_data=True,
     cmdclass={'build_py': InstallWrapper})
