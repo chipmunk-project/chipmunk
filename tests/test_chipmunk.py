0cfa9dc293b980f9e367f23924cfc554d3a70dfa
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-04-05 22:07:05 -0400

    Add another iterative solver test.

--- tests/test_chipmunk.py
+++ tests/test_chipmunk.py
@@ -124,10 +124,16 @@ class OptverifyTest(unittest.TestCase):
                       path.join(TRANSFORM_DIR, "very_simple.transform")))
 
 class IterativeSolverTest(unittest.TestCase):
-    def test_simple_sketch_2_2_raw(self):
+    def test_simple_sketch_2_2_raw_cex_mode(self):
         self.assertEqual(
             0,
             chipc.iterative_solver.main(["iterative_solver", path.join(SPEC_DIR,"simple.sk"), path.join(ALU_DIR, "raw.stateful_alu"), 2, 2, "sample1", "serial", "cex_mode"]),
+            )
+
+    def test_simple_sketch_2_2_raw_hole_elimination_mode(self):
+        self.assertEqual(
+            0,
+            chipc.iterative_solver.main(["iterative_solver", path.join(SPEC_DIR,"simple.sk"), path.join(ALU_DIR, "raw.stateful_alu"), 2, 2, "sample1", "serial", "hole_elimination_mode"]),
             )
 
 if __name__ == '__main__':
