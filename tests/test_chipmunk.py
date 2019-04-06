27af16e9da590046b14126b8983da4060240aa5f
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-04-06 10:35:28 -0400

    Add an iterative solver test for sampling_revised.sk.

--- tests/test_chipmunk.py
+++ tests/test_chipmunk.py
@@ -125,7 +125,7 @@ class OptverifyTest(unittest.TestCase):
 
 
 class IterativeSolverTest(unittest.TestCase):
-    def test_simple_sketch_2_2_raw_cex_mode(self):
+    def test_simple_2_2_raw_cex_mode(self):
         self.assertEqual(
             0,
             iterative_solver.main([
@@ -136,7 +136,7 @@ class IterativeSolverTest(unittest.TestCase):
             ]),
         )
 
-    def test_simple_sketch_2_2_raw_hole_elimination_mode(self):
+    def test_simple_2_2_raw_hole_elimination_mode(self):
         self.assertEqual(
             0,
             iterative_solver.main([
@@ -147,6 +147,16 @@ class IterativeSolverTest(unittest.TestCase):
             ]),
         )
 
+
+    def test_sampling_revised_2_2_raw_cex_mode(self):
+        self.assertEqual(
+            1,
+            iterative_solver.main([
+                "iterative_solver",
+                path.join(SPEC_DIR,"sampling_revised.sk"),
+                path.join(ALU_DIR, "raw.stateful_alu"), 2, 2, "sample1",
+                "serial", "cex_mode"]),
+            )
 
 if __name__ == '__main__':
     unittest.main()
