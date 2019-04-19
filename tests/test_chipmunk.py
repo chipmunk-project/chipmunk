56dda39081f63a0cb34cc2f4234c0c509b9c2c7e
Taegyun Kim <k.taegyun@gmail.com>
2019-04-18 23:00:46 -0400

    Re-run with autopep8.

--- tests/test_chipmunk.py
+++ tests/test_chipmunk.py
@@ -29,18 +29,10 @@ class TestDirectSolver(unittest.TestCase):
             # TODO(taegyunkim): Instead of writing to the same success and
             # failure files, use different files for each ALU.
             compiler = Compiler(
-                path.join(SPEC_DIR, "simple.sk"),
-                path.join(ALU_DIR, alu),
-                2,
-                2,
-                "simple",
-                "serial",
-            )
-            self.assertEqual(
-                compiler.serial_codegen()[0],
-                0,
-                "Compiling simple.sk failed for " + alu,
-            )
+                path.join(SPEC_DIR, "simple.sk"), path.join(ALU_DIR, alu), 2,
+                2, "simple", "serial")
+            self.assertEqual(compiler.serial_codegen()[0], 0,
+                             "Compiling simple.sk failed for " + alu)
             # TODO(taegyunkim): When all tests pass, clean up intermediary
             # files or at least have an option to keep intermediary files, with
             # default deleting them.
@@ -52,43 +44,26 @@ class TestDirectSolver(unittest.TestCase):
         with self.assertRaises(AssertionError):
             Compiler(
                 path.join(SPEC_DIR, spec_filename),
-                path.join(ALU_DIR, alu_filename),
-                1,
-                0,
-                "simple_raw_1_2",
-                "serial",
-            )
+                path.join(ALU_DIR, alu_filename), 1, 0, "simple_raw_1_2",
+                "serial")
 
     def test_simple_raw_succeeds_with_two_two_grid(self):
         spec_filename = "simple.sk"
         alu_filename = "raw.stateful_alu"
 
         compiler = Compiler(
-            path.join(SPEC_DIR, spec_filename),
-            path.join(ALU_DIR, alu_filename),
-            2,
-            2,
-            "simple_raw_2_2",
-            "serial",
-        )
+            path.join(SPEC_DIR, spec_filename), path.join(
+                ALU_DIR, alu_filename), 2, 2, "simple_raw_2_2", "serial")
         (ret_code, _, _) = compiler.serial_codegen()
         self.assertEqual(
-            ret_code,
-            0,
-            "Compiling " + spec_filename + " failed for " + alu_filename,
-        )
+            ret_code, 0,
+            "Compiling " + spec_filename + " failed for " + alu_filename)
 
         expected_holes = get_hole_dicts(
-            Path(path.join(
-                DATA_DIR,
-                "simple_raw_2_2_codegen.sk",
-            ), ).read_text(), )
+            Path(path.join(DATA_DIR, "simple_raw_2_2_codegen.sk")).read_text())
 
         output_holes = get_hole_dicts(
-            Path(path.join(
-                getcwd(),
-                "simple_raw_2_2_codegen.sk",
-            ), ).read_text(), )
+            Path(path.join(getcwd(), "simple_raw_2_2_codegen.sk")).read_text())
 
         self.assertEqual(sorted(expected_holes), sorted(output_holes))
 
@@ -97,57 +72,34 @@ class TestDirectSolver(unittest.TestCase):
         alu_filename = "raw.stateful_alu"
 
         compiler = Compiler(
-            path.join(SPEC_DIR, spec_filename),
-            path.join(ALU_DIR, alu_filename),
-            1,
-            2,
-            "simple_raw_1_2",
-            "serial",
-        )
+            path.join(SPEC_DIR, spec_filename), path.join(
+                ALU_DIR, alu_filename), 1, 2, "simple_raw_1_2", "serial")
         (ret_code, _, _) = compiler.serial_codegen()
         self.assertEqual(
-            1,
-            ret_code,
-            "Compiling " + spec_filename + " used to fail for " +
+            1, ret_code, "Compiling " + spec_filename + " used to fail for " +
             alu_filename + ", but it succeeded, please check and upate " +
-            "this test accordingly if this is expected.",
-        )
+            "this test accordingly if this is expected.")
 
     def test_test_sketch(self):
         spec_filename = "test.sk"
         alu_filename = "raw.stateful_alu"
         # Running in parallel mode to minimize test run time.
         compiler = Compiler(
-            path.join(SPEC_DIR, spec_filename),
-            path.join(ALU_DIR, alu_filename),
-            3,
-            3,
-            "test_raw_3_3",
-            "parallel",
-        )
+            path.join(SPEC_DIR, spec_filename), path.join(
+                ALU_DIR, alu_filename), 3, 3, "test_raw_3_3", "parallel")
         (ret_code, _, _) = compiler.serial_codegen()
         self.assertEqual(
-            1,
-            ret_code,
-            "Compiling " + spec_filename + " used to fail for " +
+            1, ret_code, "Compiling " + spec_filename + " used to fail for " +
             alu_filename + ", but it succeeded, please check and upate " +
-            "this test accordingly if this is expected.",
-        )
+            "this test accordingly if this is expected.")
 
         compiler = Compiler(
-            path.join(SPEC_DIR, spec_filename),
-            path.join(ALU_DIR, alu_filename),
-            4,
-            4,
-            "test_raw_4_4",
-            "parallel",
-        )
+            path.join(SPEC_DIR, spec_filename), path.join(
+                ALU_DIR, alu_filename), 4, 4, "test_raw_4_4", "parallel")
         (ret_code, _, _) = compiler.serial_codegen()
         self.assertEqual(
-            ret_code,
-            0,
-            "Compiling " + spec_filename + " failed for " + alu_filename,
-        )
+            ret_code, 0,
+            "Compiling " + spec_filename + " failed for " + alu_filename)
 
 
 class OptverifyTest(unittest.TestCase):
@@ -156,35 +108,21 @@ class OptverifyTest(unittest.TestCase):
         alu_filename = "raw.stateful_alu"
 
         compiler = Compiler(
-            path.join(SPEC_DIR, spec_filename),
-            path.join(ALU_DIR, alu_filename),
-            1,
-            1,
-            "sample1",
-            "serial",
-        )
+            path.join(SPEC_DIR, spec_filename), path.join(
+                ALU_DIR, alu_filename), 1, 1, "sample1", "serial")
 
         compiler.optverify()
 
         compiler = Compiler(
-            path.join(SPEC_DIR, spec_filename),
-            path.join(ALU_DIR, alu_filename),
-            1,
-            1,
-            "sample2",
-            "serial",
-        )
+            path.join(SPEC_DIR, spec_filename), path.join(
+                ALU_DIR, alu_filename), 1, 1, "sample2", "serial")
 
         compiler.optverify()
 
         self.assertEqual(
             0,
-            optverify(
-                "sample1",
-                "sample2",
-                path.join(TRANSFORM_DIR, "very_simple.transform"),
-            ),
-        )
+            optverify("sample1", "sample2",
+                      path.join(TRANSFORM_DIR, "very_simple.transform")))
 
 
 class IterativeSolverTest(unittest.TestCase):
@@ -194,23 +132,16 @@ class IterativeSolverTest(unittest.TestCase):
             iterative_solver.main([
                 "iterative_solver",
                 path.join(SPEC_DIR, "simple.sk"),
-                path.join(ALU_DIR, "raw.stateful_alu"),
-                "2",
-                "2",
-            ]),
+                path.join(ALU_DIR, "raw.stateful_alu"), "2", "2"]),
         )
 
     def test_simple_2_2_raw_hole_elimination_mode(self):
         self.assertEqual(
             0,
             iterative_solver.main([
-                "iterative_solver",
-                path.join(SPEC_DIR, "simple.sk"),
-                path.join(ALU_DIR, "raw.stateful_alu"),
-                "2",
-                "2",
-                "--hole-elimination",
-            ]),
+                "iterative_solver", path.join(SPEC_DIR, "simple.sk"),
+                path.join(ALU_DIR, "raw.stateful_alu"), "2", "2",
+                "--hole-elimination"]),
         )
 
     def test_sampling_revised_2_2_raw_cex_mode(self):
@@ -219,10 +150,7 @@ class IterativeSolverTest(unittest.TestCase):
             iterative_solver.main([
                 "iterative_solver",
                 path.join(SPEC_DIR, "sampling_revised.sk"),
-                path.join(ALU_DIR, "raw.stateful_alu"),
-                "2",
-                "2",
-            ]),
+                path.join(ALU_DIR, "raw.stateful_alu"), "2", "2"]),
         )
 
 
