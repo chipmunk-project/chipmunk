f3257c38c7da283fda67fba61b1b4e559ff0e0c0
Taegyun Kim <k.taegyun@gmail.com>
2019-04-28 14:21:29 -0400

    Write down list of test caes.

---
+++ tests/test_z3_utils.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+class ParseSmt2FileTest(unittest.TestCase):
+    def test_success(self):
+        pass
+
+    def test_raise_assert_for_empty_file(self):
+        pass
+
+
+class NegatedBodyTest(unittest.TestCase):
+    def test_forall(self):
+        pass
+
+    def test_variable_order(self):
+        pass
+
+
+class GenerateCounterExampleTest(unittest.TestCase):
+    def test_success(self):
+        pass
+
+    def test_unsat_formula(self):
+        pass
+
+
+class CheckWithoutBnds(unittest.TestCase):
+    def test_success(self):
+        pass
