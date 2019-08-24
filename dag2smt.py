193559b421cdf62504d30b8d1816bcf1d7cbfbb0
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-08-23 20:46:22 -0400

    First draft of generating z3 formula and checking it using z3py.

--- dag2smt.py
+++ dag2smt.py
@@ -1,103 +1,17 @@
 #! /opt/local/bin/python3
 import sys
 
-import z3
+from z3 import And
+from z3 import BoolVal
+from z3 import If
+from z3 import Int
+from z3 import IntVal
+from z3 import Or
+from z3 import Solver
+from z3 import Xor
 
-
-class UnaryOp:
-    def __init__(self, output, operand, operation):
-        self.output = '_n' + output
-        self.operand = '_n' + operand
-        self.operation = operation
-
-    def __str__(self):
-        return self.output + ' = ' + self.operation + '(' + self.operand + ')'
-
-    def __repr__(self):
-        return self.__str__()
-
-
-class BinOp:
-    def __init__(self, output, operand1, operand2, operation):
-        self.output = '_n' + output
-        self.operand1 = '_n' + operand1
-        self.operand2 = '_n' + operand2
-        self.operation = operation
-
-    def __str__(self):
-        return self.output + ' = ' + self.operation + '(' + \
-            self.operand1 + ',' + self.operand2 + ')\n'
-
-    def __repr__(self):
-        return self.__str__()
-
-
-class CondOp:
-    def __init__(self, output, predicate, yes_val, no_val):
-        self.output = '_n' + output
-        self.predicate = predicate
-        self.yes_val = '_n' + yes_val
-        self.no_val = '_n' + no_val
-
-    def __str__(self):
-        return self.output + ' = ' + self.predicate + ' ? ' + \
-            self.yes_val + ' : ' + self.no_val + '\n'
-
-    def __repr__(self):
-        return self.__str__()
-
-
-class Const:
-    def __init__(self, output, constant, var_type):
-        self.output = '_n' + output
-        self.constant = constant
-        self.var_type = var_type
-
-    def __str__(self):
-        return self.output + ' = ' + self.constant + '\n'
-
-    def __repr__(self):
-        return self.__str__()
-
-
-class Source:
-    # _type instead of type to avoid conflict with python internal type()
-    # function. The name is needed to keep track of original source variable
-    # and provide counterexamples.
-    def __init__(self, output, var_type, name):
-        self.output = output
-        self.var_type = var_type
-        assert(var_type == 'INT')
-        self.name = name
-
-    def __str__(self):
-        return self.output + ' = ' + self.name
-
-    def __repr__(self):
-        return self.__str__()
-
-
-# borrowed from getSMTnode in BooleanNodes.h
-# bool to int conversion routines
-
-
-def bool_to_int(variable):
-    return ' (ite ' + variable + ' 1 0 )'
-
-
-def int_to_bool(variable):
-    return ' (ite (> ' + variable + ' 0) true false)'
-
-
-asserts = []
-src_nodes = []
-unaryop_nodes = []
-binop_nodes = []
-cond_nodes = []
-const_nodes = []
-
-# Data structures to create z3 formula
-z3_vars = []
+# Data structures to hold z3 variables and asserts
+z3_vars = dict()
 z3_asserts = []
 
 for line in sys.stdin.readlines():
@@ -106,58 +20,61 @@ for line in sys.stdin.readlines():
     if (start in ['dag', 'TUPLE_DEF']):
         continue
     else:
-        output_var = records[0]
+        output_var = '_n' + records[0]
         operation = records[2]
-        var_type = records[3]
         if operation == 'ASSERT':
-            asserts += ['_n' + records[3]]
+            z3_asserts += ['_n' + records[3]]
         elif operation == 'S':
-            src_nodes += [Source('_n' + output_var, var_type, records[4])]
+            var_type = records[3]
+            assert(var_type == 'INT')
+            z3_vars[output_var] = Int(output_var)
         elif operation in ['NEG']:
-            unaryop_nodes += [UnaryOp(output_var, records[4], operation)]
+            z3_vars[output_var] = - z3_vars['_n' + records[4]]
         elif operation in ['AND', 'OR', 'XOR', 'PLUS',
                            'TIMES', 'DIV', 'MOD', 'LT', 'EQ']:
-            binop_nodes += [BinOp(output_var, records[4],
-                                  records[5], operation)]
+            op1 = '_n' + records[4]
+            op2 = '_n' + records[5]
+            if operation == 'AND':
+                z3_vars[output_var] = And(op1, op2)
+            elif operation == 'OR':
+                z3_vars[output_var] = Or(op1, op2)
+            elif operation == 'XOR':
+                z3_vars[output_var] = Xor(op1, op2)
+            elif operation == 'PLUS':
+                z3_vars[output_var] = z3_vars[op1] + z3_vars[op2]
+            elif operation == 'TIMES':
+                z3_vars[output_var] = z3_vars[op1] * z3_vars[op2]
+            elif operation == 'DIV':
+                z3_vars[output_var] = z3_vars[op1] / z3_vars[op2]
+            elif operation == 'MOD':
+                z3_vars[output_var] = z3_vars[op1] % z3_vars[op2]
+            elif operation == 'LT':
+                z3_vars[output_var] = z3_vars[op1] < z3_vars[op2]
+            elif operation == 'EQ':
+                z3_vars[output_var] = z3_vars[op1] == z3_vars[op2]
+            else:
+                assert(False)
         elif operation in ['ARRACC']:
-            cond_nodes += [CondOp(output_var, '_n' + records[4],
-                                  records[7], records[6])]
+            z3_vars[output_var] = If(z3_vars['_n' + records[4]],
+                                     z3_vars['_n' + records[7]],
+                                     z3_vars['_n' + records[6]])
         elif operation in ['ARRASS']:
-            cond_nodes += [CondOp(output_var, '_n' + records[4] +
-                                  ' == ' + records[6], records[8],
-                                  records[7])]
+            z3_vars[output_var] = If(z3_vars['_n' + records[4]] ==
+                                     z3_vars['_n' + records[6]],
+                                     z3_vars['_n' + records[8]],
+                                     z3_vars['_n' + records[7]])
         elif operation in ['CONST']:
-            const_nodes += [Const(output_var, records[4], var_type)]
+            var_type = records[3]
+            if var_type == 'INT':
+                z3_vars[output_var] = IntVal(int(records[4]))
+            elif var_type == 'BOOL':
+                z3_vars[output_var] = BoolVal(bool(records[4]))
+            else:
+                assert(False)
         else:
             print('unknown operation: ', line)
 
-# Now create z3 formula
-# First create z3_vars
-for node in src_nodes:
-    z3_vars += [z3.Int(node.output)]
-for node in binop_nodes:
-    if (node.operation in ['PLUS', 'TIMES', 'DIV', 'MOD']):
-        z3_vars += [z3.Int(node.output)]
-    elif (node.operation in ['AND', 'OR', 'XOR', 'LT', 'EQ']):
-        z3_vars += [z3.Bool(node.output)]
-    else:
-        assert(False), 'unknown binop node ' + node.operation
-for node in unaryop_nodes:
-    if (node.operation in ['NEG']):
-        z3_vars += [z3.Int(node.output)]
-    else:
-        assert(False), "can't handle " + node.operation
-for node in const_nodes:
-    if (node.var_type == 'INT'):
-        z3_vars += [z3.Int(node.output)]
-    else:
-        assert(node.var_type == 'BOOL')
-        z3_vars += [z3.Bool(node.output)]
-
-print('asserts:\n', asserts)
-print('src_nodes:\n', src_nodes)
-print('unaryop_nodes:\n', unaryop_nodes)
-print('binop_nodes:\n', binop_nodes)
-print('cond_nodes:\n', cond_nodes)
-print('const_nodes:\n', const_nodes)
-print('z3_vars:\n', z3_vars)
+solver = Solver()
+for constraint in z3_asserts:
+    solver.add(z3_vars[constraint])
+print(solver.check())
